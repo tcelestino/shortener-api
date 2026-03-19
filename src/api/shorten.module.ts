e3a4ce0fad9bf64003cd6ba6d7f1f7a5ff895a3f
Tiago Celestino <tcelestino@gmail.com>
2026-03-19 09:17:37 -0300

    feat: import DatabaseModule

--- src/api/shorten.module.ts
+++ src/api/shorten.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from './infra/database.module';
 import { ShortenController } from './controllers/shorten.controller';
 import { ShortenService } from './services/shorten.service';
 import { ShortenRepository } from './repositories/shorten.repository';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ShortenController],
   providers: [ShortenService, ShortenRepository],
   exports: [ShortenService],
